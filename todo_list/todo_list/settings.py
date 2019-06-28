097b958d6ad86fa85485bbcb38229c9adaf9a0e8
pokrishna <pokrishna@gmail.com>
2019-06-28 11:10:37 +0530

    ggg commit

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -37,8 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_jenkins',
     'todo_app',
 ]
+
+JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes',
+    'django_jenkins.tasks.run_jslint',
+    'django_jenkins.tasks.run_csslint',
+    'django_jenkins.tasks.run_sloccount'
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
